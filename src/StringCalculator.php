c81ea53d43fa7313b5e682dded9ceb40fb672446
jatinpatel <jatin@stealthmedia.com>
2021-09-04 14:46:28 -0600

    wip - added test cases and main logic

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -2,27 +2,45 @@
 
 namespace jatinpatel;
 
+use http\Exception\InvalidArgumentException;
 use PHPUnit\Util\Exception;
 
 class StringCalculator
 {
-    public function Add($number_string, $delimiter)
-    {
+    public function Add($number_string){
         $total = 0;
-        if (empty($number_string)){
+        if(empty($number_string)){
             return 0;
         }
-        preg_match_all('/\d+/', $number_string, $matches);
-        foreach($matches[0] as $match){
-            if ($match > 1000){ //skip if number is bigger than 1000
-                continue;
+        $delimiter = false;
+        //separate by newline to get delimiter
+        $number_string_arr = explode('\n',$number_string);
+
+        $delimiter = str_replace("//","",$number_string_arr[0]);
+        // go in if we have delimiter and size is morethan 1
+        if($delimiter && count($number_string_arr) > 1){
+            $numbers_with_delimiter = $number_string_arr[1];
+            $numbers_without_delimiter = explode($delimiter,$numbers_with_delimiter);
+            foreach ($numbers_without_delimiter as $number){
+                if($number < 0){
+                    throw new InvalidArgumentException("Negative not allowed");
+                }else if($number <= 1000){
+                    $total += $number;
+                }
             }
-            // negative number not allowed in add
-            if ($match < 0) {
-                throw new Exception("Negatives not allowed");
+        }else{
+            preg_match_all('/\d+/', $number_string, $matches);
+            foreach($matches[0] as $match){
+                if ($match > 1000){ //skip if number is bigger than 1000
+                    continue;
+                }
+                // negative number not allowed in add
+                if ($match < 0) {
+                    throw new Exception("Negatives not allowed");
+                }
+                //keep adding to total as there could be more than one number
+                $total += $match;
             }
-            //keep adding to total as there could be more than one number
-            $total += $match;
         }
         return $total;
     }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -16,12 +16,24 @@ class StringCalculatorTest extends TestCase
 
     public function testOfAdd()
     {
-        // test empty string
-        $this->assertEquals(0, $this->calculator->Add("",""));
-        // test 1
-        // test 2
+        // test empty
+        $this->assertEquals(0, $this->calculator->Add(''));
+        // test one
+        $this->assertEquals(1000, $this->calculator->Add('1000'));
         // test many
-        // test mix de
+        $this->assertEquals(8, $this->calculator->Add('1,2,5'));
+        // test with number bigger than 1000
+        $this->assertEquals(2, $this->calculator->Add("2,1001"));
+        // test with new line
+        $this->assertEquals(6, $this->calculator->Add("1\n,2,3"));
+        // test with delimiter(s)
+        $this->assertEquals(8, $this->calculator->Add("//;\n1;3;4"));
+        $this->assertEquals(6, $this->calculator->Add("//$\n1$2$3"));
+        $this->assertEquals(13, $this->calculator->Add("//@\n2@3@8"));
+       // arbitary delimiter
+        $this->assertEquals(6, $this->calculator->Add("//***\n1***2***3"));
+        // multiple delimiters
+        $this->assertEquals(6, $this->calculator->Add("//$,@\n1$2@3"));
     }
 
 }
